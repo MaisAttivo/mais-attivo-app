aca29cf0d91582549edb8574b56d8539a7063b8b
MaisAttivo <maisattivo.geral@gmail.com>
2025-09-26 21:05:36 +0100

    Create ymd.ts

---
+++ lib/ymd.ts
@@ -0,0 +1,7 @@
+// lib/ymd.ts
+export function ymdLisbon(d = new Date()) {
+  return new Intl.DateTimeFormat("en-CA", {
+    timeZone: "Europe/Lisbon",
+    year: "numeric", month: "2-digit", day: "2-digit",
+  }).format(d);
+}
